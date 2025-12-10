a4e0b68662d16d764f5c66a5de592891fbaffb15
yuriddlc <yuridokidokimnrx@gmail.com>
2025-12-10 09:26:06 +0300

    feat: add IP address checker

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-use std::net::IpAddr;
+use std::{io, net::IpAddr, process};
+
+fn get_user_input() {
+    let mut user_input = String::new();
+    println!("Введите IP:");
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => {
+            check_ip_address(user_input.trim());
+        }
+        Err(e) => {
+            eprintln!("Ошибка ввода: {}", e);
+            process::exit(1);
+        }
+    }
+}
+
 fn main() {
-    let input = "192.168.0.1";
-    match input.parse::<IpAddr>() {
+    get_user_input();
+}
+
+fn check_ip_address(user_input: &str) {
+    match user_input.parse::<IpAddr>() {
         Ok(ip) => {
-            println!("Это IP-адрес: {}", ip);
+            println!("✅ Это IP-адрес: {}", ip);
         }
         Err(_) => {
-            eprintln!("Это не IP-адрес")
+            eprintln!("❌ Это не IP-адрес");
         }
     }
 }
